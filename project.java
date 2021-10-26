4273eaa56cb1fab9587279e115c0b4d6500b7608
chakri <chakradharchakri1998@gmail.com>
2021-10-26 18:20:01 +0530

    local

---
+++ project.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\file chrome\\chromedriver.exe");
+	     WebDriver driver=new ChromeDriver(); 
+	     driver.get("https://www.amazon.in/");
+	    
+	     
+	      
+	      
+	     
+	}
+
+	
+	
+
+}
